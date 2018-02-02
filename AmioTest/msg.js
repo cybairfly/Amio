6a7a56c3a5edf4645fd5ddbd127bba7247fcfaae
Tobey <cyberfly@email.cz>
2018-02-02 10:38:53 +0100

    Update message constructors

--- AmioTest/msg.js
+++ AmioTest/msg.js
@@ -1,3 +1,4 @@
+                                                                      //Factory
 const Request = (channel, contact) => ({
     channel: {
       id: `${channel}`
@@ -27,37 +28,10 @@ module.exports = {
   Notify
 };
 
-/*
-const contact = {
-  channel: channel,
-  contact: contact
-};
-*/
-
-                                                                      //Factory
-/*
-const Request = (contact) => ({
-  channel: {
-    id: `${contact.channel}`
-  },
-  contact: {
-    id: `${contact.contact}`
-  }
-});
-
-let message = Request(contact);
-message.content = {
-    type: `text`
-};
-
-let notification = Request(contact);
-notification.type = "messages_read";
-*/
-
                                                                         //Class
 /*
 class Request {
-  constructor(channel = channel, contact = contact) {
+  constructor(channel, contact) {
     this.channel = {
       id: `${channel}`
     };
@@ -67,19 +41,30 @@ class Request {
   }
 }
 
-let message = new Request();
-message.content = {
-    "type": `text`,
-    "payload": `message`
-};
+class MessageClass extends Request {
+  constructor(channel, contact, type = `text`, payload = null) {
+    super(channel, contact);
+    this.content = {
+      type,
+      payload
+    }
+  }
+}
 
-let notification = new Request();
-notification.type = "messages_read";
+class NotifyClass extends Request {
+  constructor(channel, contact, type = `messages_read`) {
+    super(channel, contact);
+    this.type = type;
+  }
+}
+
+const Message = (channel, contact) => new MessageClass(channel, contact);
+const Notify = (channel, contact) => new NotifyClass(channel, contact);
 */
 
                                                                   //Constructor
 /*
-function Request(channel = channel, contact = contact) {
+function Request(channel, contact) {
   this.channel = {
     id: `${channel}`
   };
@@ -88,33 +73,64 @@ function Request(channel = channel, contact = contact) {
   };
 }
 
-let message = new Request();
-message.content = {
-    "type": `text`,
-    "payload": `message`
-};
-
-let notification = new Request();
-notification.type = "messages_read";
-*/
-
-                                                                //Object.create
-/*
-let request = {
-  "channel": {
-    "id": `${channel}`
-  },
-  "contact": {
-    "id": `${contact}`
+function MessageMaker(channel, contact) {
+  Request.apply(this, arguments);
+  this.content = {
+    type: `text`,
+    payload: null
   }
 }
 
-let message = Object.create(request);
-message.content = {
-    "type": `text`,
-    "payload": `message`
-};
+function NotifyMaker(channel, contact) {
+  Request.apply(this, arguments);
+  this.type = `messages_read`;
+}
 
-let notification = Object.create(request);
-notification.type = "messages_read";
+const Message = (channel, contact) => {
+  return new MessageMaker(channel, contact);
+}
+
+const Notify = (channel, contact) => {
+  return new NotifyMaker(channel, contact);
+}
 */
+
+                                                                //Object.create
+//impossible - creates empty objects with prototype from prototype template
+//new X() creates a new object with constructor X and prototype X.prototype.
+//Object.create(X) creates a new object with prototype X (and therefore constructor X.constructor)
+
+                                                                        //POJO
+/*
+const Request = (channel, contact) => {
+  const request = {
+    "channel": {
+      "id": `${channel}`
+    },
+    "contact": {
+      "id": `${contact}`
+    }
+  }
+  return request;
+}
+
+const Message = (channel, contact) => {
+  const message = Request(channel, contact);
+  message.content = {
+      "type": `text`,
+      "payload": `message`
+  };
+  return message;
+}
+
+const Notify = (channel, contact) => {
+  const notification = Request(channel, contact);
+  notification.type = `message_read`;
+  return notification;
+}
+*/
+
+module.exports = {
+  Message,
+  Notify
+};
