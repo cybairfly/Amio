4f718f57b73165e44a883188f13815e1ee6bb30b
Tobey <cyberfly@email.cz>
2018-02-03 16:23:05 +0100

    logs

--- ChatBot/index.js
+++ ChatBot/index.js
@@ -1,1 +1,79 @@
-console.log('OK');//test
+'use strict';
+console.log('started');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+console.log('required');
+//const port = process.env.PORT || 443;
+// const options = {
+//     key: fs.readFileSync( './ssl/localhost.key' ),
+//     cert: fs.readFileSync( './ssl/localhost.cert' ),
+//     requestCert: false,
+//     rejectUnauthorized: false
+// };
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+console.log('required');
+const _http = http.createServer(app);
+const _https = https.createServer(options, app);
+console.log('served');
+_http.listen(80, () => console.log(`Listening on: ${_http.address().port}`));
+//_https.listen(443, () => console.log(`Listening on: ${_https.address().port}`));
+console.log('listening');
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "Cryptolaemusmontrouzieri"
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
