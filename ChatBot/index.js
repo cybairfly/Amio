4b9cdd0e95dbd187ab5a09ce9b2a1e3ba626079b
Tobey <cyberfly@email.cz>
2018-02-09 18:08:22 +0100

    bot

--- ChatBot/index.js
+++ ChatBot/index.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const options = {
+    key: fs.readFileSync( './ssl/private.key' ),
+    cert: fs.readFileSync( './ssl/certificate.crt' ),
+    ca: [ fs.readFileSync('./ssl/ca_bundle.crt') ],
+    requestCert: false,
+    rejectUnauthorized: false
+};
 
 // Imports dependencies and set up http server
 const
@@ -13,13 +19,17 @@ const
   app = express().use(bodyParser.json()); // creates express http server
 
 const _http = http.createServer(app);
-// const _https = https.createServer(options, app);
+const _https = https.createServer(options, app);
 
-_http.listen(port, () => console.log(`Listening on: ${_http.address().port}`));
+const httpPort = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80;
+const httpsPort = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 443;
+
+_http.listen(httpPort, () => console.log(`Listening on: ${_http.address().port}`));
+_https.listen(httpsPort, () => console.log(`Listening on: ${_https.address().port}`));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-
+  console.log(`post webhook called`);
   let body = req.body;
 
   // Checks this is an event from a page subscription
@@ -43,16 +53,21 @@ app.post('/webhook', (req, res) => {
 
 });
 
+app.get('/', (req, res) => {
+      res.status(200).send('meh');
+});
+
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "Cryptolaemusmontrouzieri"
+  let VERIFY_TOKEN = "EAAGUjD32ZABABANYS4ePNG5ReWkhcSmIZCmUIADSZBsTq54dzr2IuEWDdhssWC5o7ZBnw2IFmMzfjbeXgTDg4e5mjwfiAe3k9tbeN51MhciE2lJsfkHX46MzsvEEu5QrIDLsnkqnSRNpb5sTIXdw0IIiZCDady9JVKQTvxBTqrwZDZD";
 
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
+  console.log(mode, token, challenge);
 
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
